67d028d5d4875b1a80b3a7ec2cd38d83b64438e3
Anirudh Gupta <anirudhgupta109@gmail.com>
2019-07-30 23:38:55 +0530

    [DNM] CustomDoze: Change TiltSesor to oneplus.sensor.pickup
    Signed-off-by: Anirudh Gupta <anirudhgupta109@gmail.com>
    Change-Id: I5a7c8bfc12de9e2372d7cf54592b7b9b11352d93

--- src/com/custom/ambient/display/TiltSensor.java
+++ src/com/custom/ambient/display/TiltSensor.java
@@ -48,7 +48,14 @@ public class TiltSensor implements SensorEventListener {
         mPowerManager = (PowerManager) mContext.getSystemService(Context.POWER_SERVICE);
         mSensorManager = (SensorManager) mContext.getSystemService(Context.SENSOR_SERVICE);
         if (mSensorManager != null) {
-            mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_TILT_DETECTOR);
+            String oemPocket = mContext.getResources().getString(
+                                        R.string.config_custom_tilt);
+            if (!oemPocket.isEmpty()) {
+                mSensorManager = mContext.getSystemService(SensorManager.class);
+                mSensor = Utils.getSensor(mSensorManager, oemPocket);
+            } else {
+                mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_TILT_DETECTOR);
+            }
         }
         mSensorWakeLock = mPowerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK,
                 "SensorWakeLock");
