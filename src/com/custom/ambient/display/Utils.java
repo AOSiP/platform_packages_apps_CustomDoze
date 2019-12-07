ca9b03617befb6708d7615d171716dc6e22d88c9
Marko Man <darkobas@gmail.com>
2019-12-08 02:10:13 +0530

    CustomDoze: Add separate config for pulse brightness [2/2]
    Change-Id: I8c47cd665ec234f503e45d0770b4408a8a3c9681

--- src/com/custom/ambient/display/Utils.java
+++ src/com/custom/ambient/display/Utils.java
@@ -44,6 +44,8 @@ public final class Utils {
     protected static final String GESTURE_HAND_WAVE_KEY = "gesture_hand_wave";
     protected static final String GESTURE_POCKET_KEY = "gesture_pocket";
     protected static final String MUSIC_TICKER_KEY = "pulse_on_new_tracks";
+    protected static final String OMNI_DOZE_BRIGHTNESS_KEY = "doze_brightness";
+    protected static final String OMNI_PULSE_BRIGHTNESS_KEY = "pulse_brightness";
 
     protected static void startService(Context context) {
         if (DEBUG) Log.d(TAG, "Starting service");
